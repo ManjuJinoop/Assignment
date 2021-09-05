b6d864f38bfdd84bbf4c7812793bbe4c2f8943a8
ManjuJinoop <manjumanidharan@gmail.com>
2021-09-05 15:58:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import UploadImages from "./components/upload-files.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <h4>React Image Upload with Preview</h4>
+
+      <div className="content">
+        <UploadImages />
+      </div>
     </div>
   );
 }
